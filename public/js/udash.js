f66ba1d1c57c5caafbcc2a00f8ceb9615953b9e0
Alex Weidner <side.weidner@gmail.com>
2014-09-09 12:38:39 -0400

    woo we add dummy widgets to our first iteration of the canvas...

--- public/js/udash.js
+++ public/js/udash.js
@@ -7,8 +7,22 @@ $(function(){
     widget_margins: [5, 5],
     draggable: {
       handle: 'header'
-    }
+    },
+    max_cols: 8
   }).data('gridster');
+
+  $('#addwidget').on('click', function(e) {
+    $('#widgetmodal').modal('show');
+  });
+
+  $('.addwidget-btn').on('click', function(e) {
+    var numwidgets = $('.widgetnum').val();
+    for (i = 0; i < numwidgets; i++) {
+      gridster.add_widget("<li class='new'><header>|||</header><p>I am a brand new widget.  Gosh, I feel so fresh and so clean.  Won't you drag me around?</p></li>", 1, 2);
+    }
+    $('.gs_w').css('background-color', '#DDD');
+    $('#widgetmodal').modal('hide');
+  });
 
   // awful hacky shit because the god damn css provided with the gridster demo
   // apparently does not fucking work and cannot get my own css to apply
