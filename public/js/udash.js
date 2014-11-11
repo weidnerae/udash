7cbec3a802c8f622256d365b8d6923fede968ba5
Alex Weidner <side.weidner@gmail.com>
2014-11-11 18:16:16 -0500

    starting to really get deals view going, added new elements for getting businesses so we can group deals by business as well

--- public/js/udash.js
+++ public/js/udash.js
@@ -7,9 +7,16 @@ $(function(){
     });
   });
 
-  // click eat on the sidebar
-  $('#eat').on("click", function(e) {
-    // load the eat.html into #view div
-    $('#view').load( "eat.html" );
+  // click deals on the sidebar
+  $('#deals').on("click", function(e) {
+    // first we want to see if the view is tagged with 'deals-active', indicating
+    // that it's already been loaded into the #view div.  we don't wanna reload.
+    if (!$('#view').hasClass('.deals-active')) {
+      // load the deals.html into #view div
+      $('#view').load( "deals.html" );
+      // add class that lets us know that deals are active
+      $('#view').removeClass();
+      $('#view').addClass('.deals-active');
+    }
   });
 });
