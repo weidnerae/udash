44a25c8576aafc9b696dd8bb103dfb7710c12282
Alex Weidner <side.weidner@gmail.com>
2014-12-10 16:46:04 -0500

    changing listening port to 80

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express  = require('express')
 var request  = require('request')
-var Cloudant = require('cloudant')
 var config   = require('./config.json')
+var nano     = require('nano')('http://localhost:5984')
+
+var oneDay   = 1000 * 60 * 60 * 24;
 
 // setup middleware
 // testing github crap
@@ -9,8 +11,8 @@ var app = express()
 app.use(express.bodyParser())
 app.use(app.router)
 app.use(express.errorHandler())
-app.use(express.static(__dirname + '/public')) //setup static public directory
-app.use(express.static(__dirname + '/bower_components'))
+app.use(express.static(__dirname + '/public', { maxAge: oneDay })) //setup static public directory
+app.use(express.static(__dirname + '/bower_components', { maxAge: oneDay}))
 app.set('view engine', 'jade')
 app.set('views', __dirname + '/views') //optional since express defaults to CWD/views
 
@@ -24,38 +26,9 @@ var oauth = {
 	token_secret: config.token_secret
 }
 
-// cloudant database stuff!
-var me = config.username
-var password = config.password
-
 // app specific data structures
 var deals = [];
 var bizs = [];
-
-// initialize connection to cloudant
-Cloudant({account:me, password:password}, function(er, cloudant) {
-	console.log('========== CONNECTING TO CLOUDANT ==========')
-	if (er)
-		return console.log("Error connecting to cloudant account: %s: %s", me, er.message)
-
-	console.log("Connected to cloudant account: %s", me)
-
-	cloudant.ping(function(er, reply) {
-		if (er)
-			return console.log('Failed to ping cloudant, did your network just go down?')
-
-		console.log('Server version = %s', reply.version)
-		console.log('I am %s and my roles are %j', reply.userCtx.name, reply.userCtx.roles)
-
-		cloudant.db.list(function(er, all_dbs) {
-			if (er)
-				return console.log('Error listing all databases: %s', er.message)
-
-				console.log('Databases: %s', all_dbs.join(','))
-				console.log('========== DONE CONNECTING TO CLOUDANT =========')
-		})
-	})
-})
 
 // render index page
 app.get('/', function(req, res){
@@ -114,16 +87,24 @@ app.get('/deals', function(req, res) {
 
 app.get('/deals2', function(req, res) {
 	deals = [];
-	Cloudant({account: me, password: password}, function(error, cloudant) {
-		var db = cloudant.db.use('deals2')
-		db.list({ include_docs: true }, function (err, body) {
-			body.rows.forEach(function (element) {
-				deals.push(element.doc)
-			})
-			console.log("deals grabbed from db: " + deals.length)
-			res.json(deals)
+	var db = nano.db.use('deals')
+	db.list({ include_docs: true}, function (err, body) {
+		body.rows.forEach(function (element) {
+			deals.push(element.doc)
 		})
+		console.log("deals grabbed from db: " + deals.length)
+		res.json(deals)
 	})
+	// Cloudant({account: me, password: password}, function(error, cloudant) {
+	// 	var db = cloudant.db.use('deals2')
+	// 	db.list({ include_docs: true }, function (err, body) {
+	// 		body.rows.forEach(function (element) {
+	// 			deals.push(element.doc)
+	// 		})
+	// 		console.log("deals grabbed from db: " + deals.length)
+	// 		res.json(deals)
+	// 	})
+	// })
 })
 
 app.get('/bizs', function(req, res) {
@@ -141,4 +122,4 @@ app.get('/bizs', function(req, res) {
 	})
 })
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 80)
