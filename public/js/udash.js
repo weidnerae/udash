8e026b6566bebc0086aff0b5273a952bab2c2d49
Alex Weidner <side.weidner@gmail.com>
2014-12-10 21:44:51 -0500

    fixing changing views

--- public/js/udash.js
+++ public/js/udash.js
@@ -6,12 +6,4 @@ $(function(){
       drawerPanel.togglePanel();
     });
   });
-
-  // click deals on the sidebar
-  $('#deals').on("click", function(e) {
-    $('#view').load("deals.html");
-  });
-  $('#places').on("click", function(e) {
-    $('#view').load("bizs.html");
-  });
 });
